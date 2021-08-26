02b4235783a3db9a0baed200dfd35a8905b17b24
sen329 <hans.hilman@gmail.com>
2021-08-26 15:01:44 +0700

    fix openSQL

--- Controller/openSql.go
+++ Controller/openSql.go
@@ -25,8 +25,8 @@ func Open() (dbase *sql.DB) {
 	username := goDotEnvVariable("DB_USER_NAME")
 	password := goDotEnvVariable("DB_PASSWORD")
 	address := goDotEnvVariable("DB_ADDRESS")
-	// database := goDotEnvVariable("DB_DATABASE")
-	dbase, err := sql.Open(driver, username+":"+password+"@tcp("+address+")"+"/")
+	database := goDotEnvVariable("DB_DATABASE")
+	dbase, err := sql.Open(driver, username+":"+password+"@tcp("+address+")"+"/"+database)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -34,12 +34,12 @@ func Open() (dbase *sql.DB) {
 }
 
 func OpenGMAdmin() (dbase *sql.DB) {
-	driver := goDotEnvVariable("DB_DRIVER")
-	username := goDotEnvVariable("DB_USER_NAME_ADMIN")
-	password := goDotEnvVariable("DB_PASSWORD_ADMIN")
-	address := goDotEnvVariable("DB_ADDRESS_ADMIN")
-	database := goDotEnvVariable("DB_DATABASE_ADMIN")
-	dbase, err := sql.Open(driver, username+":"+password+"@tcp("+address+")"+"/"+database)
+	driverGM := goDotEnvVariable("DB_DRIVER")
+	usernameGM := goDotEnvVariable("DB_USER_NAME_ADMIN")
+	passwordGM := goDotEnvVariable("DB_PASSWORD_ADMIN")
+	addressGM := goDotEnvVariable("DB_ADDRESS_ADMIN")
+	databaseGM := goDotEnvVariable("DB_DATABASE_ADMIN")
+	dbase, err := sql.Open(driverGM, usernameGM+":"+passwordGM+"@tcp("+addressGM+")"+"/"+databaseGM)
 	if err != nil {
 		panic(err.Error())
 	}
