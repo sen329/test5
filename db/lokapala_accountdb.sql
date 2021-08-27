f04cb524fcacab345aa7f0aeae9226c13db4ab39
sen329 <hans.hilman@gmail.com>
2021-08-27 13:07:14 +0700

    change database

--- db/lokapala_accountdb.sql
+++ db/lokapala_accountdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2021 at 07:47 AM
+-- Generation Time: Aug 27, 2021 at 08:05 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
