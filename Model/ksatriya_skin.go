b020848de0a296a009fab06ac1a46aeb289f324f
sen329 <hans.hilman@gmail.com>
2021-08-31 17:37:07 +0700

    fix ksatriya skin null value error

--- Model/ksatriya_skin.go
+++ Model/ksatriya_skin.go
@@ -1,7 +1,7 @@
 package model
 
 type Ksatriya_skin struct {
-	Ksatriya_skin_id int64  `json:"ksatriya_skin_id"`
-	Ksatriya_id      int64  `json:"ksatriya_id"`
-	Release_date     string `json:"release_date"`
+	Ksatriya_skin_id int64   `json:"ksatriya_skin_id"`
+	Ksatriya_id      int64   `json:"ksatriya_id"`
+	Release_date     *string `json:"release_date"`
 }
