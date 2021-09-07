40395083ef1eb526b9a1603f96a83e9bc197e6e7
sen329 <hans.hilman@gmail.com>
2021-09-07 14:54:24 +0700

    fix code positioning

--- Model/player_user.go
+++ Model/player_user.go
@@ -11,3 +11,31 @@ type Player struct {
 	Playing_time int     `json:"playing_time"`
 	Frame        int     `json:"frame"`
 }
+
+type Player_Ksatriya_ranking struct {
+	User_id      int64 `json:"user_id"`
+	Ksatriya_id  int64 `json:"ksatriya_id"`
+	Win_count    int   `json:"win_count"`
+	Lose_count   int   `json:"lose_count"`
+	Match_count  int   `json:"match_count"`
+	Win_rate     int   `json:"win_rate"`
+	Rank         int   `json:"rank"`
+	Country_rank int   `json:"country_rank"`
+}
+
+type Player_match_history struct {
+	Room_id               int64  `json:"room_id"`
+	User_id               *int64 `json:"user_id"`
+	Win                   int    `json:"win"`
+	Ksatriya_id           int64  `json:"ksatriya_id"`
+	Level                 int    `json:"level"`
+	Kill                  int    `json:"kill"`
+	Death                 int    `json:"death"`
+	Assist                int    `json:"assist"`
+	Gold                  int    `json:"gold"`
+	Damage_dealt          int    `json:"damage_dealt"`
+	Damage_taken          int    `json:"damage_taken"`
+	Ksatriya_damage_dealt int    `json:"ksatriya_damage_dealt"`
+	Game_duration         int    `json:"game_duration"`
+	Game_mode             *int   `json:"game_mode"`
+}
