3c093f7ed651389a3306ab9f4f205cf00dae1d42
sen329 <hans.hilman@gmail.com>
2021-09-09 14:43:48 +0700

    fix error

--- Model/ksatriya_skin.go
+++ Model/ksatriya_skin.go
@@ -3,6 +3,6 @@ package model
 type Ksatriya_skin struct {
 	Ksatriya_skin_id *int64  `json:"ksatriya_skin_id"`
 	Ksatriya_id      *int64  `json:"ksatriya_id"`
-	Ksatriya_name    *int64  `json:"ksatriya_name"`
+	Ksatriya_name    *string `json:"ksatriya_name"`
 	Release_date     *string `json:"release_date"`
 }
