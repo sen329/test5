86aff136f08da71a2384f8be541984f2407c9d6c
sen329 <hans.hilman@gmail.com>
2021-09-24 17:27:24 +0700

    Fix model matches

--- Model/matches.go
+++ Model/matches.go
@@ -1,12 +1,12 @@
 package model
 
 type Matches struct {
-	Room_id     int64  `json:"room_id"`
-	Room_name   string `json:"room_name"`
-	Match_id    int64  `json:"match_id"`
-	Game_mode   int    `json:"game_mode"`
-	Server_ip   string `json:"server_ip"`
-	Server_port int    `json:"server_port"`
-	Start_time  string `json:"start_time"`
-	Can_timeout string `json:"can_timeout"`
+	Room_id     int64   `json:"room_id"`
+	Room_name   string  `json:"room_name"`
+	Match_id    int64   `json:"match_id"`
+	Game_mode   *int    `json:"game_mode"`
+	Server_ip   *string `json:"server_ip"`
+	Server_port *int    `json:"server_port"`
+	Start_time  *string `json:"start_time"`
+	Can_timeout *string `json:"can_timeout"`
 }
